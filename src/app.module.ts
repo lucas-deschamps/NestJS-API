fa1443cbe778a95eba5e197a6d129cb1c141deff
Lucas Deschamps <l-deschamps@outlook.com>
2020-10-06 19:47:22 -0300

    adicionando código para conexão ao banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
 import { AddressModule } from './address/address.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [StudentModule, AddressModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), StudentModule, AddressModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,7 +5,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   host: 'localhost',
   port: 5432,
   username: 'postgres',
-  password: 'postgres',
+  password: 'postgres', // estou usando outra senha na minha API localmente, mas subindo a padrão do postgres
   database: 'bancodedados',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
