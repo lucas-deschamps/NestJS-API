1d0f4926375c2737e60c0490c85acc211a7091a8
Lucas Deschamps <l-deschamps@outlook.com>
2020-10-06 16:17:15 -0300

    gerando módulo address, referente ao endereço

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
+import { AddressModule } from './address/address.module';
 
 @Module({
-  imports: [StudentModule],
+  imports: [StudentModule, AddressModule],
   controllers: [AppController],
   providers: [AppService],
 })
