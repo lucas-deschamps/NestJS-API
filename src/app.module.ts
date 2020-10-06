921a36d64557e675f26be3687d5f634239c66127
Lucas Deschamps <l-deschamps@outlook.com>
2020-10-06 15:39:14 -0300

    deletando módulo para usar nomes em inglês, para maior consistência

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AlunoModule } from './aluno/aluno.module';
 
 @Module({
-  imports: [AlunoModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
