32cfcfb78a9002d0630cf33e4897c955e5cbabc2
Lucas Deschamps <l-deschamps@outlook.com>
2020-10-06 17:48:49 -0300

    injetando service referente ao endereço no controller do mesmo

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { AddressService } from './address.service';
 
 @Controller('/endereco')
-export class AddressController {}
+export class AddressController {
+  constructor(private addressService: AddressService) {}
+};
