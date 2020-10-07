ad4b5f0c8d7836a13f9726407bacfffdd7239e1a
Lucas Deschamps <l-deschamps@outlook.com>
2020-10-06 22:58:08 -0300

    injetando repositório student (aluno) no módulo referente ao mesmo (student.module)

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentController } from './student.controller';
 import { StudentService } from './student.service';
+import { StudentRepository } from './student.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([StudentRepository])],
   controllers: [StudentController],
   providers: [StudentService]
 })
