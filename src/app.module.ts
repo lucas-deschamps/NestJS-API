ed3d29ed330aa27ee0774871323d90c72da789db
Lucas Deschamps <l-deschamps@outlook.com>
2020-10-06 21:45:39 -0300

    removendo arquivos desnecessários para a API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
 import { AddressModule } from './address/address.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -8,7 +6,5 @@ import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmConfig), StudentModule, AddressModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
