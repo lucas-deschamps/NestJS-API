67d61fccfc8b2caf3207e5e5b31870b2f93a639f
Lucas Deschamps <l-deschamps@outlook.com>
2020-10-06 23:04:08 -0300

    injetando repositório address (endereço) no módulo referente ao mesmo (address.module)

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AddressController } from './address.controller';
 import { AddressService } from './address.service';
+import { AddressRepository } from './address.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AddressRepository])],
   controllers: [AddressController],
   providers: [AddressService]
 })
