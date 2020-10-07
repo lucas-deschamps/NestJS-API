1ff7244a33c72c2dc4afe30ea5c001a4e015a120
Lucas Deschamps <l-deschamps@outlook.com>
2020-10-06 22:45:11 -0300

    criando repositório (student.repository) referente à entidade aluno (student)

---
+++ src/student/student.repository.ts
@@ -0,0 +1,5 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Student } from './student.entity';
+
+@EntityRepository(Student)
+export class StudentRepository extends Repository<Student> {};
