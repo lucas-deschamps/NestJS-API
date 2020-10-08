4464cb07c6df56a212804820f5f981074cc00b50
Lucas Deschamps <l-deschamps@outlook.com>
2020-10-07 22:19:46 -0300

    implementação do método getStudentById (GET), referente ao endpoint /aluno/:id

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { StudentDto } from './dto/student.dto';
 import { Student } from './student.entity';
@@ -6,6 +6,11 @@ import { Student } from './student.entity';
 @Controller('/aluno')
 export class StudentController {
   constructor(private studentService: StudentService) {}
+
+  @Get('/:id')
+  getStudentById(@Param('id') id: number): Promise<Student> {
+    return this.studentService.getStudentById(id);
+  };
 
   @Post()
   createStudent(@Body() studentDto: StudentDto): Promise<Student> {
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { StudentRepository } from './student.repository';
 import { StudentDto } from './dto/student.dto';
@@ -7,6 +7,16 @@ import { Student } from './student.entity';
 @Injectable()
 export class StudentService {
   constructor(@InjectRepository(StudentRepository) private studentRepository: StudentRepository) {}
+
+  async getStudentById(id: number): Promise<Student> {
+    const student = await this.studentRepository.findOne(id);
+
+    if (!student) {
+      throw new NotFoundException(`O aluno com o ID "${id}" não foi encontrado.`);
+    };
+
+    return student;
+  };
 
   async createStudent(studentDto: StudentDto): Promise<Student> {
     return this.studentRepository.createStudent(studentDto);
